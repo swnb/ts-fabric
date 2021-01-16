5adbd8e0b107e275fd20208552fbe7b72043c6e5
swnb <swnblovego@gmail.com>
2021-01-16 22:26:26 +0800

    style(eslint): format

--- src/eslint.ts
+++ src/eslint.ts
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: [require.resolve("@umijs/fabric/dist/eslint")],
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
   rules: {
-    "no-plusplus": [2, { allowForLoopAfterthoughts: true }],
-    "consistent-return": 0,
-    "@typescript-eslint/consistent-type-definitions": 0,
-    "lines-between-class-members": 1,
-    "@typescript-eslint/member-ordering": 1,
+    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'consistent-return': 0,
+    '@typescript-eslint/consistent-type-definitions': 0,
+    'lines-between-class-members': 1,
+    '@typescript-eslint/member-ordering': 1,
     eqeqeq: 0,
   },
 }
