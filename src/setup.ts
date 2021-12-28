38881291406495e16ef01f325e301ecd25c9b2cf
swnb <swnblovego@gmail.com>
2021-12-28 10:48:15 +0800

    feat(src/setup): write file only if file doesn't exist when automaticCall setup

--- src/setup.ts
+++ src/setup.ts
@@ -4,21 +4,31 @@ import type { Stats } from 'fs'
 const fs = require('fs')
 const path = require('path')
 
+let isAutomaticCall = false
+
 let currentWorkDirPath = process.argv[2]
 if (!currentWorkDirPath) {
+  isAutomaticCall = true
   currentWorkDirPath = '.'
 }
 
 // 删除原来的配置文件
-const originConfigPath = path.resolve(currentWorkDirPath, '.prettierrc')
-fs.stat(originConfigPath, (err: Error | null, stats: Stats) => {
-  if (err) return
-  if (stats.isFile()) {
-    fs.unlink(originConfigPath, (err: Error | null) => {
-      if (err) return
-      console.log('delete .prettierrc')
-    })
-  }
+
+function removeFile(file: string) {
+  const originConfigPath = path.resolve(currentWorkDirPath, file)
+  fs.stat(originConfigPath, (err: Error | null, stats: Stats) => {
+    if (err) return
+    if (stats.isFile()) {
+      fs.unlink(originConfigPath, (unlinkError: Error | null) => {
+        if (unlinkError) return
+        console.log('delete .prettierrc')
+      })
+    }
+  })
+}
+
+;['.prettierrc'].forEach(file => {
+  removeFile(file)
 })
 
 const prettierConfigPath = path.resolve(currentWorkDirPath, '.prettierrc.js')
@@ -29,14 +39,17 @@ module.exports = {
   ...prettier,
 };
 `
-fs.createWriteStream(prettierConfigPath).write(prettierConfigContent, (err: Error | null) => {
-  if (err) {
-    console.error(`can't creat .prettierrc.js in ${prettierConfigPath}`)
-    return
-  }
 
-  console.log('success create .prettierrc.js file')
-})
+if (!fs.existsSync(prettierConfigPath) || !isAutomaticCall) {
+  fs.createWriteStream(prettierConfigPath).write(prettierConfigContent, (err: Error | null) => {
+    if (err) {
+      console.error(`can't creat .prettierrc.js in ${prettierConfigPath}`)
+      return
+    }
+
+    console.log('success create .prettierrc.js file')
+  })
+}
 
 const eslintConfigPath = path.resolve(currentWorkDirPath, '.eslintrc')
 const eslintConfigContent = `
@@ -44,11 +57,14 @@ const eslintConfigContent = `
   "extends": "./node_modules/@swnb/fabric/dist/eslint"
 }
 `
-fs.createWriteStream(eslintConfigPath).write(eslintConfigContent, (err: Error | null) => {
-  if (err) {
-    console.error(`can't creat ..eslintrc in ${eslintConfigPath}`)
-    return
-  }
 
-  console.log('success create .eslintrc file')
-})
+if (!fs.existsSync(eslintConfigContent) || isAutomaticCall) {
+  fs.createWriteStream(eslintConfigPath).write(eslintConfigContent, (err: Error | null) => {
+    if (err) {
+      console.error(`can't creat ..eslintrc in ${eslintConfigPath}`)
+      return
+    }
+
+    console.log('success create .eslintrc file')
+  })
+}
