6b92a036077561f34c9c0460d747647b99871305
swnb <swnblovego@gmail.com>
2021-12-28 10:29:05 +0800

    chore

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,28 +1,6 @@
-/** @format */
+
+const prettier = require("@swnb/fabric/dist/prettier");
 
 module.exports = {
-  singleQuote: true,
-  jsxSingleQuote: true,
-  trailingComma: 'all',
-  printWidth: 100,
-  semi: false,
-  useTabs: false,
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: 'avoid',
-  endOfLine: 'lf',
-  overrides: [
-    {
-      files: '.prettierrc',
-      options: {
-        parser: 'json',
-      },
-    },
-    {
-      files: 'document.ejs',
-      options: {
-        parser: 'html',
-      },
-    },
-  ],
-}
+  ...prettier,
+};
